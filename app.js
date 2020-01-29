459eff2948cfb4b8946f92d5377a1ed8bf6573e0
Chris <52025505+chrisleerod@users.noreply.github.com>
2020-01-29 12:30:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ var toggleMenu = gsap.timeline({ paused: true });
 
 welcome
     .from(".welcome-h", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, .25)
-    .from(".welcome-p", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5,}, .50)
-    .from(".welcome-thanks", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5,}, .75)
+    .from(".welcome-p", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, .50)
+    /*.from(".welcome-thanks", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, .75)
     .from(".learn-more-p", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5,}, .75)
     .from(".arrow", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, .75)
-    .from(".projects-h", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, 1)
+    .from(".projects-h", {ease: Power4.easeOut, y: 50, opacity: 0, duration: 1.5}, 1)*/
 
 
 gsap.to(".arrow", {x: 15, repeat: -1, yoyo: true,});
@@ -49,42 +49,3 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('js-x');
   nextStateMap[isReversed]();
 })
-
-// helper function to determine if an element is in view
-const isInViewport = el => {
-  let top = el.offsetTop;
-  let left = el.offsetLeft;
-  let width = el.offsetWidth;
-  let height = el.offsetHeight;
-
-  while(el.offsetParent) {
-    el = el.offsetParent;
-    top += el.offsetTop;
-    left += el.offsetLeft;
-  }
-
-  return (
-    top < (window.pageYOffset + window.innerHeight) &&
-    left < (window.pageXOffset + window.innerWidth) &&
-    (top + height) > window.pageYOffset &&
-    (left + width) > window.pageXOffset
-  );
-};
-
-const grid = document.querySelector('.container-grid');
-const projects = document.getElementsByClassName('project-container');
-
-window.addEventListener('scroll', function (event) {
-  if (isInViewport(grid)) {
-    for(let i = 0; i < projects.length; i++) {
-      if (projects[i].classList.contains('project-animation')) {
-        projects[i].className = "project-container noop";
-        return;
-      }
-
-      if (!projects[i].classList.contains('noop')) {
-        projects[i].className += " project-animation"; 
-      }
-    }
-	}
-}, false);
